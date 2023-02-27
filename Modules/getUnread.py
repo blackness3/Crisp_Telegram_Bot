af496cfc13fe9d4866b01ad2e379961f1e711c31
moefaq <i@faq.moe>
2023-02-27 18:04:09 +0000

    1. 修改消息获取方式     原有逻辑从会话中搜索含有未读消息的会话，并返回last_message。     修改为：从会话中搜索含有未读消息的会话，遍历会话中的消息，按照text消息和file消息进行分类发送至telegram 2. 添加图片接收功能

--- Modules/getUnread.py
+++ Modules/getUnread.py
@@ -13,21 +13,50 @@ class Conf:
 
 async def exec(context: ContextTypes.DEFAULT_TYPE):
     website_id = config['crisp']['website']
-    messages = client.website.search_conversations(website_id, 1, filter_unread='1')
-    if len(messages) > 0:
+    conversations = client.website.search_conversations(
+        website_id, 1, filter_unread='1')
+    if len(conversations) > 0:
         data = {
             "from": "user",
             "origin": "chat",
+            "fingerprints": []
         }
-        for i in messages:
-            session_id = i['session_id']
-            client.website.mark_messages_read_in_conversation(website_id, session_id, data)
-            text = '📠Crisp消息推送\n'
-            messages = i['last_message']
-            text = f'{text}🧾内容：{messages}\n\n'
-            text = f'{text}🧷Session：{session_id}'
-            for admin_id in config['bot']['admin_id']:
-                await context.bot.send_message(
-                    chat_id=admin_id,
-                    text=text
-                )
+        query = {
+
+        }
+        for conversation in conversations:
+            session_id = conversation['session_id']
+            # Crisp api docs: Returns the last batch of messages. 这个last batch到底能有多少我没整明白.
+            messages = client.website.get_messages_in_conversation(
+                website_id, conversation['session_id'], query)
+            for message in messages:
+                # read长度为0时该条消息未读
+                if len(message['read']) == 0:
+                    # 筛选出文本消息
+                    if message['type'] == 'text':
+                        #通过消息指纹将消息置为已读
+                        data['fingerprints'] = [message['fingerprint']]
+                        client.website.mark_messages_read_in_conversation(
+                            website_id, session_id, data)
+
+                        text = '📠Crisp消息推送\n'
+                        content = message['content']
+                        text = f'{text}🧾内容：{content}\n\n'
+                        text = f'{text}🧷Session：{session_id}'
+                        for admin_id in config['bot']['admin_id']:
+                            await context.bot.send_message(
+                                chat_id=admin_id,
+                                text=text
+                            )
+                    # 筛选出文件类型消息
+                    if message['type'] == 'file':
+                        # 通过文件mime type筛选出含image消息
+                        mime = str(message['content']['type'])
+                        if mime.count('image') > 0:
+                            #通过消息指纹将消息置为已读
+                            data['fingerprints'] = [message['fingerprint']]
+                            client.website.mark_messages_read_in_conversation(
+                                website_id, session_id, data)
+                            
+                            for admin_id in config['bot']['admin_id']:
+                                await context.bot.send_photo(chat_id=admin_id, photo=message['content']['url'], caption=message['content']['name'])
